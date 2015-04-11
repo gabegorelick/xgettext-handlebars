8f94576e719d945250346bbf820593e94fb69b7c
Gabe Gorelick <gabegorelick@gmail.com>
2015-04-11 16:30:18 -0400

    Reformat code

--- index.js
+++ index.js
@@ -78,94 +78,89 @@ Parser.messageToKey = function (msgid, msgctxt) {
  * @return Object The list of translatable strings, the line(s) on which each appears and an optional plural form.
  */
 Parser.prototype.parse = function (template) {
-    var keywordSpec = this.keywordSpec,
-      keywords = Object.keys(keywordSpec),
-      tree = Handlebars.parse(template);
+  var keywordSpec = this.keywordSpec;
+  var keywords = Object.keys(keywordSpec);
+  var tree = Handlebars.parse(template);
 
-    var isMsg = function (msgs, statement) {
-        statement = statement.sexpr || statement;
+  var collectMsgs = function (msgs, statement) {
+    statement = statement.sexpr || statement;
 
-        switch (statement.type) {
-          case 'sexpr':
-            if (keywords.indexOf(statement.id.string) !== -1) {
-              var spec = keywordSpec[statement.id.string],
-                params = statement.params,
-                msgidParam = params[spec.indexOf('msgid')];
+    if (statement.type === 'sexpr') {
+      if (keywords.indexOf(statement.id.string) !== -1) {
+        var spec = keywordSpec[statement.id.string];
+        var params = statement.params;
+        var msgidParam = params[spec.indexOf('msgid')];
 
-              if (msgidParam) { // don't extract {{gettext}} without param
-                var msgid = msgidParam.string,
-                  contextIndex = spec.indexOf('msgctxt');
+        if (msgidParam) { // don't extract {{gettext}} without param
+          var msgid = msgidParam.string;
+          var contextIndex = spec.indexOf('msgctxt');
 
-                var context = null; // null context is *not* the same as empty context
-                if (contextIndex >= 0) {
-                  var contextParam = params[contextIndex];
-                  if (!contextParam) {
-                    // throw an error if there's supposed to be a context but not enough
-                    // parameters were passed to the handlebars helper
-                    throw new Error('No context specified for msgid "' + msgid + '"');
-                  }
-                  if (contextParam.type !== 'STRING') {
-                    throw new Error('Context must be a string literal for msgid "' + msgid + '"');
-                  }
+          var context = null; // null context is *not* the same as empty context
+          if (contextIndex >= 0) {
+            var contextParam = params[contextIndex];
+            if (!contextParam) {
+              // throw an error if there's supposed to be a context but not enough
+              // parameters were passed to the handlebars helper
+              throw new Error('No context specified for msgid "' + msgid + '"');
+            }
+            if (contextParam.type !== 'STRING') {
+              throw new Error('Context must be a string literal for msgid "' + msgid + '"');
+            }
 
-                  context = contextParam.string;
+            context = contextParam.string;
+          }
+
+          var key = Parser.messageToKey(msgid, context);
+          msgs[key] = msgs[key] || {line: []};
+
+          // make sure plural forms match
+          var pluralIndex = spec.indexOf('msgid_plural');
+          if (pluralIndex !== -1) {
+            var pluralParam = params[pluralIndex];
+            if (!pluralParam) {
+              throw new Error('No plural specified for msgid "' + msgid + '"');
+            }
+            if (pluralParam.type !== 'STRING') {
+              throw new Error('Plural must be a string literal for msgid ' + msgid);
+            }
+
+            var plural = pluralParam.string;
+            var existingPlural = msgs[key].msgid_plural;
+            if (plural && existingPlural && existingPlural !== plural) {
+                throw new Error('Incompatible plural definitions for msgid "' + msgid +
+                  '" ("' + msgs[key].msgid_plural + '" and "' + plural + '")');
+            }
                 }
 
-                var key = Parser.messageToKey(msgid, context);
-                msgs[key] = msgs[key] || {line: []};
+            msgs[key].line.push(statement.firstLine);
 
-                // make sure plural forms match
-                var pluralIndex = spec.indexOf('msgid_plural');
-                if (pluralIndex !== -1) {
-                  var pluralParam = params[pluralIndex];
-                  if (!pluralParam) {
-                    throw new Error('No plural specified for msgid "' + msgid + '"');
-                  }
-                  if (pluralParam.type !== 'STRING') {
-                    throw new Error('Plural must be a string literal for msgid ' + msgid);
-                  }
-
-                  var plural = pluralParam.string;
-                  var existingPlural = msgs[key].msgid_plural;
-                  if (plural && existingPlural && existingPlural !== plural) {
-                    throw new Error('Incompatible plural definitions for msgid "' + msgid +
-                      '" ("' + msgs[key].msgid_plural + '" and "' + plural + '")');
-                  }
-                }
-
-                msgs[key].line.push(statement.firstLine);
-
-                spec.forEach(function(prop, i) {
-                  var param = params[i];
-                  if (param && param.type === 'STRING') {
-                    msgs[key][prop] = params[i].string;
-                  }
-                });
-
-                // maintain backwards compatibility with plural output
-                msgs[key].plural = msgs[key].msgid_plural;
+            spec.forEach(function(prop, i) {
+              var param = params[i];
+              if (param && param.type === 'STRING') {
+                msgs[key][prop] = params[i].string;
               }
-            }
+            });
 
-            statement.params.reduce(isMsg, msgs);
-
-            break;
-          case 'block':
-            if (statement.program) {
-              statement.program.statements.reduce(isMsg, msgs);
-            }
-
-            if (statement.inverse) {
-              statement.inverse.statements.reduce(isMsg, msgs);
-            }
-
-            break;
+            // maintain backwards compatibility with plural output
+            msgs[key].plural = msgs[key].msgid_plural;
+          }
         }
 
-        return msgs;
-      };
+        statement.params.reduce(collectMsgs, msgs);
+      } else if (statement.type === 'block') {
+        if (statement.program) {
+          statement.program.statements.reduce(collectMsgs, msgs);
+        }
 
-    return tree.statements.reduce(isMsg, {});
+        if (statement.inverse) {
+          statement.inverse.statements.reduce(collectMsgs, msgs);
+        }
+      }
+
+      return msgs;
+    };
+
+    return tree.statements.reduce(collectMsgs, {});
   };
 
 module.exports = Parser;
